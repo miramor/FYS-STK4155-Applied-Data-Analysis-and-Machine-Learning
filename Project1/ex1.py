ee1e43aa301f5cd7a109c2b95f971fe0954c04a5
miram <miram@student.matnat.uio.no>
2021-10-08 10:59:04 +0200

    add print statement in ex.1

--- Project1/ex1.py
+++ Project1/ex1.py
@@ -23,22 +23,23 @@ X = create_X(x,y,complex)
 
 test_train_l = train_test_split(X,z,test_size=0.2)
 #Exercise 1
-print(f"OLS: {evaluate_method(ols, test_train_l, scale = False, d = 5)}")
-print(f"OLS: {evaluate_method(ols, test_train_l, scale = True, d = 5)}")
+print("OLS for (MSE test, R2 test, MSE train, MSE train)")
+print(f"OLS without noise (not scaled): {evaluate_method(ols, test_train_l, scale = False, d = 5)}")
+print(f"OLS without noise (scaled): {evaluate_method(ols, test_train_l, scale = True, d = 5)}")
 
 noise = np.random.normal(0, 1, size=(z.shape))
-z_noisy = FrankeFunction(x, y) + noise*0.1
+z_noisy = FrankeFunction(x, y) + noise*0.2
 test_train_l_noise = train_test_split(X,z_noisy,test_size=0.2)
-print(f"OLS with noise: {evaluate_method(ols, test_train_l_noise, scale = False, d = 5)}")
-print(f"OLS with noise: {evaluate_method(ols, test_train_l_noise, scale = True, d = 5)}")
+print(f"OLS with noise (not scaled): {evaluate_method(ols, test_train_l_noise, scale = False, d = 5)}")
+print(f"OLS with noise (scaled): {evaluate_method(ols, test_train_l_noise, scale = True, d = 5)}")
 variance_beta = var_beta(test_train_l_noise[0])
 beta_l = ols(test_train_l_noise[0], test_train_l_noise[2])
 confidence_interval = ci(beta_l, variance_beta, N)
 
 beta_sd_l = variance_beta*(1.96/np.sqrt(N))
-print(len(beta_l))
-print(beta_sd_l)
-print(np.log(beta_l))
+#print(len(beta_l))
+#print(beta_sd_l)
+#print(np.log(beta_l))
 
 labelsize=21
 ticksize = 19
@@ -50,4 +51,5 @@ plt.ylabel(r"log(abs($\beta$))", fontsize=labelsize)
 plt.xticks(fontsize=ticksize)
 plt.yticks(fontsize=ticksize)
 plt.grid()
+plt.savefig("beta_CI.png")
 plt.show()
